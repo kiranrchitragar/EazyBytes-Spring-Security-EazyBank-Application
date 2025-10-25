674c8a165c645e34bf77cb8f77dc06714e538f0a
Kiran Chitragar <kiranphdt@gmail.com>
2025-10-25 14:45:49 +0530

    JDBC Userdetails manager and Custom User details manager

--- eazybank-application/src/main/java/com/eazybank/configuration/JdbcUserDetailsManagerSecurityConfig.java
+++ eazybank-application/src/main/java/com/eazybank/configuration/JdbcUserDetailsManagerSecurityConfig.java
@@ -2,21 +2,15 @@ package com.eazybank.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+public class JdbcUserDetailsManagerSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -43,30 +37,17 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        /*
-        if not using password encoder use {noop} -> .password("{noop}12345")
+//      As we are using Custom Userdetails manager, we need to comment this - EazyBankUserDetailsManager
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
-        https://bcrypt-generator.com/
-        Password is 12345, use the above link to encrpt the pswd
-         */
-        UserDetails user = User.withUsername("user").password("{noop}Blue!Tiger@47Sun").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$kDn2lQ/lobS3.Ucwlv6CBeIfDAskRXWSBBv6JIO1pQYTekeI6dBJe").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user,admin);
-    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         // By default if we use factories, it uses - BCryptPasswordEncoder
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    // Check if the password is compromised, Check if the password in
-    // weak or is a simple password.
-    @Bean
-    public CompromisedPasswordChecker compromisedPasswordChecker(){
-        // Changing password from 12345-> to strong pasword Blue!Tiger@47Sun
-        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
