e27e1924d69f797607dfb85a5f84d73bb3482bff
Kiran Chitragar <kiranphdt@gmail.com>
2025-10-29 23:43:30 +0530

    Authorities Using roles and Authorities

--- eazybank-application/src/main/java/com/eazybank/configuration/ProjectSecurityConfig.java
+++ eazybank-application/src/main/java/com/eazybank/configuration/ProjectSecurityConfig.java
@@ -1,15 +1,19 @@
 package com.eazybank.configuration;
 
+import com.eazybank.exceptionhandling.CustomAccessDeniedHandler;
+import com.eazybank.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import com.eazybank.filters.CSRFCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -26,78 +30,56 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-
         CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
-
-        http.cors(corsCustomizer->corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setMaxAge(3600L);
-                return config;
-            }
-        }));
-
-/*
-        -- Commenting this to check CSRF issues
-        http.sessionManagement(smc->smc.invalidSessionUrl("/invalidSession")// Once time out redirect to this page
-                .maximumSessions(1)  // Maximum Sessions a user can have. This will invalidate the previous session and new session is cretaed. To avoid this we use below
-                .maxSessionsPreventsLogin(true)); // This wont allow for second session to be created till the 1st session is timedout or expired.
- */
-
-        http.requiresChannel(rcc->rcc.anyRequest().requiresInsecure()); // Allows only http calls
-        // http.csrf(csrf->csrf.disable());
-
-        // CSRF is applicable only for POST/PUT/DELET calls for private pages- meaning pages visible after login to application
-        // A CsrfTokrnRepository persists the CSRF token in a cookie named "XSRF-TOKEN" and reads from header "X-XSRF-TOKEN".
-        http.csrf(csrfConfig->csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler) // The CsrfTokenRequestAttributeHandler stores the CSRF token as a request attribute (so it can be accessed from filters or templates
-                .ignoringRequestMatchers("/contact","/registerUsers") // Ignore CSRF for public pages, without login
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())); //Save the CSRF token in a cookie (named XSRF-TOKEN by default).Allow JavaScript code (like Angular, React, etc.) to read that token — since HttpOnly is set to false.
-        http.addFilterAfter(new CSRFCookieFilter(), BasicAuthenticationFilter.class);
-
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/user","/notices", "/contact", "/error","/registerUser").permitAll());
-
-
-        // This tells the Spring, we are not storing the session information in Security Context , while it can save these details in Security COntext.
-        http.securityContext(contextConfig->contextConfig.requireExplicitSave(false));
-
-        // We are telling the Spring security framework - Always create a session for us, so we can use this session again & again to access secured API's
-        http.sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
-
-        /* Disable form login, if disabled we can use only API's via API's
-           http.formLogin(httpSecurityFormLoginConfigurer -> {
-           httpSecurityFormLoginConfigurer.disable();
-        });
-         */
-
-        /* Disable basic login, if disabled we can use only API's via forms
-           http.httpBasic(httpSecurityHttpBasicConfigurer -> {
-           httpSecurityHttpBasicConfigurer.disable();
-        });
-         */
+        http.securityContext(contextConfig -> contextConfig.requireExplicitSave(false))
+                .sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers( "/contact","/registerUser")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CSRFCookieFilter(), BasicAuthenticationFilter.class)
+                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure()) // Only HTTP
+                .authorizeHttpRequests((requests) -> requests
+                       .requestMatchers("/myAccount").hasAuthority("VIEWACCOUNT")
+                        .requestMatchers("/myBalance").hasAnyAuthority("VIEWBALANCE", "VIEWACCOUNT")
+                        .requestMatchers("/myLoans").hasAuthority("VIEWLOANS")
+                        .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
+                        /*.requestMatchers("/myAccount").hasRole("USER")
+                       .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                       .requestMatchers("/myLoans").hasRole("USER")
+                       .requestMatchers("/myCards").hasRole("USER")*/
+                        .requestMatchers("/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/registerUser", "/invalidSession").permitAll());
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
-//      As we are using Custom Userdetails manager, we need to comment this - EazyBankUserDetailsManager
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        // By default if we use factories, it uses - BCryptPasswordEncoder
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    /**
+     * From Spring Security 6.3 version
+     *
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
 }
